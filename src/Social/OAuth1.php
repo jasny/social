08bbe19c71b49963359874a76777064d6475ec6b
Arnold Daniels <arnold@jasny.net>
2013-01-09 06:28:30 +0100

    Fixed oauth::asUser

--- src/Social/OAuth1.php
+++ src/Social/OAuth1.php
@@ -73,7 +73,7 @@ abstract class OAuth1 extends Connection
      */
     public function asUser($access, $accessSecret=null)
     {
-        return new self($this->appId, $this->appSecret, $access, $accessSecret);
+        return new static($this->appId, $this->appSecret, $access, $accessSecret);
     }
     
     
